8a75bcc043d70069a058f1a0cdec5bd50d0ccaff
Naga Monish Munagala <52042001+nagamonish@users.noreply.github.com>
2023-03-23 13:46:07 +0000

    Updated the home page

--- my-app/src/components/home/HeaderSocials.jsx
+++ my-app/src/components/home/HeaderSocials.jsx
@@ -3,16 +3,8 @@ import React from 'react'
 const HeaderSocials = () => {
   return (
     <div className="home__socials">
-      <a href="https://www.instagram.com" className="home__social-link" target = "_blank">
-        <i className="fa-brands fa-instagram"></i>
-      </a>
-
-      <a href="https://www.twitter.com" className="home__social-link" target = "_blank">
-        <i className="fa-brands fa-twitter"></i>
-      </a>
-
-      <a href="https://www.behance.com" className="home__social-link" target = "_blank">
-        <i className="fa-brands fa-behance"></i>
+      <a href="https://www.github.com" className="home__social-link" target = "_blank">
+        <i className="fa-brands fa-github"></i>
       </a>
 
       <a href="https://www.dribbble.com" className="home__social-link" target = "_blank">
